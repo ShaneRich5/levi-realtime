4e5d6be87ec941920c26b2427bf4fbd48803d0a2
Shane Richards <shane.richards121@gmail.com>
2017-07-30 20:24:47 -0500

    switched protocol to https

--- app.js
+++ app.js
@@ -1,10 +1,20 @@
-var server = require('http').Server();
+var https = require('https');
+var fs = require('fs');
+
+var options = {
+  key: fs.readdirSync('/etc/nginx/ssl/api.levihq.com/226909/server.key'),
+  cert: fs.readdirSync('/etc/nginx/ssl/api.levihq.com/226909/server.crt')
+};
+
+var server = https.createServer(options);
+
+// var server = require('http').Server();
 var io = require('socket.io')(server);
 var Redis = require('ioredis');
 var redis = new Redis();
 
-redis.subscribe('levi-notifications', function(err, count) {
-});
+
+redis.subscribe('levi-notifications', function(err, count) {});
 
 redis.on('message', function(channel, message) {
     console.log('Message Recieved: ' + message);
@@ -12,6 +22,10 @@ redis.on('message', function(channel, message) {
     message = JSON.parse(message);
 		console.log(channel + ':' + message.event);
     io.emit(channel + ':' + message.event, message.data);
+});
+
+io.on('connection', function(socket) {
+  console.log('a connection was established');
 });
 
 server.listen(3000, function(){
